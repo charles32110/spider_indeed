70505317a784f15f210e46f8dd31415cb767f0b5
charles32110 <596147986@qq.com>
2019-01-20 23:50:46 +1300

    change to py3.6 get the picture

--- analy_py.py
+++ analy_py.py
@@ -1,9 +1,21 @@
-#coding:utf-8
+
 import pandas as pd
 import nltk
 import jieba
 import wordcloud
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib
+matplotlib.use('Qt5Agg')
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pylab
 
 
+
+
+pypath = './testpy.csv'
+df = pd.read_csv(pypath,encoding = "ISO-8859-1")
+df['jobtitle'].value_counts().plot.bar()#column
+plt.show()
+
+#print showdf[['jobtitle','jobtype']]#columns
